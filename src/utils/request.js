c929197ae0647cdd6be7100e32a9065b7a81cd1a
wangyupo <1106774262@qq.com>
2024-06-03 18:56:02 +0800

    [^]axios拦截错误

--- src/utils/request.js
+++ src/utils/request.js
@@ -37,11 +37,19 @@ request.interceptors.response.use(
   function (response) {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
+    if (response.data.code != 0) {
+      debounceErr(response.data.msg);
+    }
+
     return response.data;
   },
   function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    if (error.response.status == 401) {
+      debounceErr(error.response.data.msg);
+      loginOut();
+    }
     return Promise.reject(error);
   }
 );
